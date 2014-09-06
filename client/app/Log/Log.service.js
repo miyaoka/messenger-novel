5e69b839bfb87f12da29d8524b02f3610162f596
Masaya Kazama <miyan@t-p.jp>
2014-09-07 04:24:46 +0900

    1ページに表示するログの行数を制限するようにした

--- client/app/Log/Log.service.js
+++ client/app/Log/Log.service.js
@@ -4,12 +4,18 @@ angular.module('messengerApp')
   .factory('Log', function () {
     var Log= {
       list: [],
+      maxLength: 100,
       clear: function(){
         Log.list = [];
       },
       add: function(msg){
         msg.time = new Date();
         Log.list.push(msg);
+
+        //最大表示行数を超えたら先頭から消去
+        while(Log.list.length > Log.maxLength){
+          Log.list.shift();
+        }
       }
     }
 
