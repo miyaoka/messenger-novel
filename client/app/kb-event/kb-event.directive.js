876dc3be2a2347b45588a8142d379cc74404e58e
Masaya Kazama <miyan@t-p.jp>
2014-09-07 00:13:48 +0900

    キーボードイベントをリファクタリング。enterキーでもトークするように追加

--- client/app/kb-event/kb-event.directive.js
+++ client/app/kb-event/kb-event.directive.js
@@ -5,10 +5,25 @@ angular.module('messengerApp')
     return {
       restrict: 'E',
       replace: true,
-      scope: true,
-      link:    function postLink(scope, iElement, iAttrs){
-        jQuery(document).on('keydown', function(e){
-          scope.$apply(scope.keyDown(e));
+      scope: {
+        keypress: '&',
+        keydown: '&',
+        keyup: '&'
+      },
+      link: function (scope, element, attrs){
+        scope.$on('$destroy', function(){
+          $(document).off('keypress keydown keyup');
+        });
+        $(document).on({
+          'keypress': function(e){
+            scope.$apply(scope.keypress({$event: e}));
+          },
+          'keydown': function(e){
+            scope.$apply(scope.keydown({$event: e}));
+          },
+          'keyup': function(e){
+            scope.$apply(scope.keyup({$event: e}));
+          }
         });
       }
     };
--- client/app/messenger/messenger.controller.js
+++ client/app/messenger/messenger.controller.js
@@ -3,30 +3,33 @@
 angular.module('messengerApp')
   .controller('MessengerCtrl', function ($scope, $timeout, Message, Log, $state) {
 
+    var self = this;
     this.msgs = Message;
     this.logs = Log;
 
-    $scope.keyDown = function(e) {
-      //本state以外でもキーイベントを拾ってしまうのでその場合は破棄する
-      if(!$state.is('main.messenger')){
-        return;
-      }
-      //32: space
-      //40: arrow down
-      if(e.which == 32 || e.which == 40){
-        $scope.say();
-      }
-    };
 
-    $scope.say = function(){
-      var msg = Message.next();
-      if(!msg){
+    this.say = function(){
+      if(!Message.next()){
         return;
       }
       $('#msgr-scroll').stop();
       $('#msgr-scroll').animate({
         scrollTop: document.getElementById('msgr-scroll').scrollHeight
       }, 300);
+    }
 
+    this.keyPress = function(e){
+      //13: enter
+      //32: space
+      if(e.which == 13 || e.which == 32){
+        self.say();
+      }
+    }
+    //arrow keyはpressで取得できないのでdownから
+    this.keyDown = function(e){
+      //40: arrow down
+      if(e.which == 40){
+        self.say();
+      }
     }
   });
